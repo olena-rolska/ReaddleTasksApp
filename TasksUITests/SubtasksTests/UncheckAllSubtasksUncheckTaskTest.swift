989b8bb1c8bc49f2ad6dff4bfe6dc4abb4d8833b
olena-rolska <manitskayaelena@gmail.com>
2023-04-06 14:57:57 +0300

    added uncheck subtasks uncheks the task status step added

---
+++ TasksUITests/SubtasksTests/UncheckAllSubtasksUncheckTaskTest.swift
@@ -0,0 +1,52 @@
+//
+//  UncheckAllSubtasksUncheckTaskTest.swift
+//  TasksUITests
+//
+//  Created by Test on 06.04.2023.
+//  Copyright © 2023 Cultured Code. All rights reserved.
+//
+
+import XCTest
+
+class UncheckAllSubtasksUncheckTaskTest: BaseTest {
+    let email = "dummy@gmail.com"
+    let password = "1"
+    
+    let taskCompleted = "Selected"
+    let taskUnchecked = "Not selected"
+    
+    override func tearDown() {
+        deleteApp()
+        super.tearDown()
+    }
+    
+    func testUncheckAllSubtasksUncheckTask() throws {
+        let loginScreen = LoginScreen()
+        loginScreen.login(email: email, pass: password)
+
+        let tasksScreen = TasksScreen()
+        XCTAssert(tasksScreen.tasksPage.waitForExistence(timeout: 5), "User is not logged in")
+        
+        let sleepTaskIndex: Int = 3
+        let sleepTask = app.tables.cells.element(boundBy: sleepTaskIndex)
+        
+        sleepTask.tap()
+        
+        tasksScreen.checkTaskStatus(expectedStatus: sleepTask.images["cell_image_view"].value, actualStatus: taskCompleted)
+        
+        let subtasksScreen = SubTasksScreen()
+        subtasksScreen.moreInfoButton.tap()
+        XCTAssert(subtasksScreen.subtasksPageTitle.waitForExistence(timeout: 2), "User is not on the subtasks screen")
+        
+        tasksScreen.manageAllTasks(option: .complete)
+        tasksScreen.manageAllTasks(option: .uncheck)
+        tasksScreen.checkAllTasksStatuses(status: taskUnchecked)
+        
+        subtasksScreen.backButton.tap()
+        
+        XCTAssert(tasksScreen.tasksPage.waitForExistence(timeout: 5), "User is on the wrong screen")
+        
+        tasksScreen.checkTaskStatus(expectedStatus: sleepTask.images["cell_image_view"].value, actualStatus: taskUnchecked)
+        
+    }
+}
